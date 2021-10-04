503c505da1edb24ab6e00cfcf80bbebcb72959ce
Ehmarcus <91565835+Ehmarcus@users.noreply.github.com>
2021-10-04 15:03:07 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Initialization
+import turtle
+ezra=turtle.Turtle()
+ezra.left(90)
+import random
+#Functions
+def randomDot():
+    ezra.color("purple")
+    ezra.begin_fill()
+    ezra.end_fill()
+    ezra.circle(200)
+
+#Main
+randomDot()
